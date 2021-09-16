94e614c8aab95435b1c25d9e76745a0e55c83c71
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-09-16 21:43:34 +0100

    book controller and route to pass first book test

--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const router = express.Router()
+const bookCtrl = require('../controllers/book')
 
+router.post('/', bookCtrl.create)
+
+router.get('/', bookCtrl.read)
 
 
 
