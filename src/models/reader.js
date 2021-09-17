917d96d67799e02bafc4e3613ef0715918d9f41b
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-09-17 18:27:27 +0100

    added validation to reader model

--- src/models/reader.js
+++ src/models/reader.js
@@ -1,8 +1,22 @@
 module.exports = (connection, DataTypes) => {
     const schema = {
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            isEmail: true
+        },
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            len: 8
+        }
     };
 
     const ReaderModel = connection.define('Reader', schema);
