13fb8582fa2c33c40d1d6795a08d99b60b07975c
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-09-17 18:31:27 +0100

    added validation to book model

--- src/models/book.js
+++ src/models/book.js
@@ -1,9 +1,22 @@
 module.exports = (connection, DataTypes) => {
     const schema = {
-        title: DataTypes.STRING,
-        author: DataTypes.STRING,
-        genre: DataTypes.STRING,
-        ISBN: DataTypes.STRING
+
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        author:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        genre: {
+            type: DataTypes.STRING,},
+
+        ISBN: {
+            type: DataTypes.STRING,
+            unique: true,
+            isNumeric: true
+        },
 
     }
     const BookModel = connection.define('Book', schema)
